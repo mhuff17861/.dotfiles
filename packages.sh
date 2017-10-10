6d80c4d5f41cfa570944062f49e66ed89dd8cc2c
Andrew Lang <andrew.lang19@gmail.com>
2017-10-10 16:06:40 -0400

    Stuff

--- packages.sh
+++ packages.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-apt-get install \
+sudo apt-get install \
 	feh \
+	gem \
 	git \
 	i3 i3-wm i3lock i3status \
+	python-pip python-setuptools \
 	vim \
 	zsh
+
+python -m ensurepip --user
+python -m pip install --user --upgrade pip
+python -m pip install --user --upgrade virtualenv
+gem install tmuxinator
